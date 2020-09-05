983daf884e6418dcb90daefb17e9a52e618a5187
Anteshwar <tondareanteshwar@gmail.com>
2020-09-06 03:03:03 +0530

    Updates

---
+++ precache-manifest.67d159f40770a53df714a24b3da3abe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3933a7171abe754bba09caf60b349a",
+    "url": "/movies-series-catalog/index.html"
+  },
+  {
+    "revision": "e0ae092caae15d84612d",
+    "url": "/movies-series-catalog/static/css/main.f96b3293.chunk.css"
+  },
+  {
+    "revision": "82268d2e40e891e30a96",
+    "url": "/movies-series-catalog/static/js/2.6f522358.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-series-catalog/static/js/2.6f522358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ae092caae15d84612d",
+    "url": "/movies-series-catalog/static/js/main.582b2a5e.chunk.js"
+  },
+  {
+    "revision": "4037156a70ccbaa70aea",
+    "url": "/movies-series-catalog/static/js/runtime-main.ad6b148c.js"
+  },
+  {
+    "revision": "325cad7fbc9362857a6f2c2332404582",
+    "url": "/movies-series-catalog/static/media/Spinner.325cad7f.gif"
+  }
+]);
